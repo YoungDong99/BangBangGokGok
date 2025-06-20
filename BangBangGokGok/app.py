94ae43563cdc3ca72e11d573e1a33fe9513e91f9
YoungDong99 <114642786+YoungDong99@users.noreply.github.com>
2025-06-20 10:20:08 +0900

    실행파일 수정

--- BangBangGokGok/app.py
+++ BangBangGokGok/app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, Blueprint
+from flask import redirect, url_for
+from controller.MainController import main
 
 app = Flask(__name__)
+app.register_blueprint(main)
 
 # 기본 페이지
 @app.route('/')
 def home():
-    return "hello world"
+    return redirect(url_for("main.index"))
 
 
 # 서버 실행
